0be9ad1b6bf6b44144c1b09619cee3d535e122bc
shendo7 <rodneyrjames@gmail.com>
2019-08-08 19:08:26 -0700

    added LinkedIn

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+$(document).ready(function (){
+
 $('#contactMe').on('click', function () {
     $('#myInput').trigger('focus')
   })
+
+let LinkedIn = "http://linkedin.com/in/rodney-james-0a35a0104"
+
+$('#btnLinkedIn').on('click', function () {
+  $('#btnLinkedIn').html('LinkedIn')
+
+  })})
